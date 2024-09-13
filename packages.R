1c871a41b51b1bc1cd8280a9df8d82dd1afb7707
LW <whiteman.70@osu.edu>
2024-09-13 15:16:30 -0400

    packages

---
+++ packages.R
@@ -0,0 +1,11 @@
+#### packages ####
+library(readxl)
+library(ggplot2)
+library(ggstatsplot)
+library(tidyverse)
+library(tidyr)
+library(ggpubr)
+library(viridis)
+library(emmeans)
+library(dplyr)
+library(fifer) #chi-square post hoc
