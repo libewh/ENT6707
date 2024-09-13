fe286a4fde41f9f0ceea953b4b0cd0a7e654d1ac
LW <whiteman.70@osu.edu>
2024-09-13 16:09:05 -0400

    updates

--- Ento6707.R
+++ Ento6707.R
@@ -1,8 +1,14 @@
 ento <- read_csv("Ent6707.csv")
 
 summary(ento)
+str(ento)
 head(ento)
 tail(ento)
+
+
+
+ento$species <- as.factor(ento$species)
+ento$distance_km <- as.factor(ento$distance_km)
 
 ##create table with distances and parasite counts ###
 
@@ -12,3 +18,14 @@ tail(ento)
 
 # checking for normality using a density plot #
 ## WILL NOT WORK WITH PRESENCE/ABSENCE ##
+
+
+
+plot(conopid ~ species, family = binomial, data = ento)
+
+
+
+glm(nosema ~ distance_km + species, family = binomial, data = ento)
+glm(apicystis ~ distance_km + species, family = binomial, data = ento)
+glm(crithidia ~ distance_km + species, family = binomial, data = ento)
+
