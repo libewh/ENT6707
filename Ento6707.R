9975ac98847ccd2f57c789ceb34d51297caf7da9
LW <whiteman.70@osu.edu>
2024-09-23 10:59:06 -0400

    update

--- Ento6707.R
+++ Ento6707.R
@@ -1,20 +1,31 @@
+#############################################################
+#############################################################
+#############################################################
+
 ento <- read_excel("Ent6707.xlsx")
 
 # how many bee species in the data set?
 ento$species <- as.factor(ento$species)
 levels(ento$species)
+summary(ento$species)
 
 # what are the distances in the data set?
 ento$distance_km <- as.factor(ento$distance_km)
 levels(ento$distance_km)
+summary(ento$distance_km)
 
 # how many plant genera in the data set?
 ento$plantgen <- as.factor(ento$plantgen)
 levels(ento$plantgen)
+summary(ento$plantgen)
 
+# how many conopids in the data set?
+ento$conopid <- as.factor(ento$conopid)
+summary(ento$conopid)
 
 str(ento)
 summary(ento)
+summary(ento$species)
 
 
 ########   PLOTS   ##########################################
@@ -24,13 +35,13 @@ summary(ento)
 ggplot(ento) +
   aes(x = distance_km, fill = species) +
   geom_bar(position = "stack") +
-  xlab("distance from apiary") +
+  xlab("distance from apiary (km)") +
   ylab("count")
 
 # add column for species count 
-df <- ento %>% group_by(species) %>%   mutate(count_name_occurr = n())
+df <- ento %>% group_by(species) %>% mutate(count_name_occurr = n())
 
-# bees by count
+# count of bees
 ggplot(df) +
   aes(x = reorder(species, -count_name_occurr), fill = species) +
   geom_bar(position = "stack") +
@@ -46,3 +57,18 @@ ggplot(df2) +
   geom_bar(position = "stack") +
   xlab("flowers") +
   ylab("count")
+
+
+# subset pathogens
+paths <- ento[, c("apicystis", "crithidia", "nosema")]
+print(paths)
+
+df.pca <- rda(paths)
+df.pca
+summary(df.pca)
+
+(inventory <- df.pca$CA$eig)
+screeplot(df.pca, bstick=TRUE, npcs=length(df.pca$CA$eig))
+
+par(mfrow=c(1,1))
+biplot(df.pca, scaling=1, type="text", xlab="poop", ylab="pee")
